a7e7be27a7760780a0e9a87404bfe402dea10cb0
Lucille Ablett <lucilleablett@gmail.com>
2020-01-30 10:25:23 +0000

    Updates

---
+++ precache-manifest.cedb6d02374d1d5a52744889bd407599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3672cd6fe082172e3534d8663629b2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fd119bbe7b98aef45616",
+    "url": "/robofriends/static/css/main.css"
+  },
+  {
+    "revision": "fd119bbe7b98aef45616",
+    "url": "/robofriends/static/js/main.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/main.js.LICENSE"
+  },
+  {
+    "revision": "615d283446bced077a7824155c85e3e0",
+    "url": "/robofriends/static/media/FakeHope.615d2834.woff"
+  }
+]);
